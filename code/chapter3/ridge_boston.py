5f2022bebe427cb0d133eaa467e4c4d2add09730
rnorm <zhang.nl@outlook.com>
2019-06-20 22:12:12 -0400

    Delete ridge_boston.py

--- code/chapter3/ridge_boston.py
+++
@@ -1,9 +0,0 @@
-from sklearn.datasets import load_boston
-from linear_regression_ridge import LR_Ridge
-boston = load_boston()
-X, y = boston.data, boston.target
-# first, let's run our own linear regression
-ridge = LR_Ridge(0.5)
-ridge.fit(X, y)
-print(ridge.coef)
-print(ridge.predict(X)[:5])
